fba0d916a53e0794989393f9cffa54926343b57a
Сергей <sergeym83@bk.ru>
2025-12-23 15:49:20 +0300

    methods start

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,67 @@
+import java.time.LocalDate;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-
-
+    public static boolean checkingLeapYear(int date) {// check year
+        if (date < 1584) {
+            return false;
+        } else if (date % 400 == 0 || (date % 4 == 0 && date % 100 != 0)) {
+            return true;
+        } else {
+            return false;
+        }
     }
+
+    public static void CheckingUserPhone(int OS, int year) {
+        int currentYear = LocalDate.now().getYear();
+        if (year >= currentYear && OS == 0) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        } else if (OS == 0) {
+            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+        } else if (year >= currentYear && OS == 1) {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        } else if (OS == 1) {
+            System.out.println("Установите облегченную версию приложения для Android по ссылке");
+        }
+    }
+
+    public static int CalculateDeliveryTime(int distance) {
+        if (distance <= 20) {
+            return 1;
+        } else if (distance <= 60) {
+            return 2;
+        } else {
+            return 3;
+        }
+    }
+
+
+    public static void main(String[] args) {
+        //задача 1
+        System.out.println("задача 1");
+        int year = 2021;
+        if (checkingLeapYear(year)) {
+            System.out.println(year + " год является високосным");
+        } else {
+            System.out.println(year + " год не является високосным");
+        }
+
+        //задача 2
+        System.out.println("задача 2");
+        int clientOS = 0;
+        int clientDeviceYear = 2016;
+        CheckingUserPhone(clientOS, clientDeviceYear);
+
+        //задача 3
+        System.out.println("задача 3");
+        int deliveryDistance = 95;
+        int deliveryTime = CalculateDeliveryTime(deliveryDistance);
+        if (deliveryDistance > 100) {
+            System.out.println("Доставки нет");
+        } else {
+            System.out.println("Потребуется дней: " + deliveryTime);
+        }
+    }
+
 }
