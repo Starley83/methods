41c38c870084451757d72bc78fabbb687fcea7b9
Сергей <sergeym83@bk.ru>
2025-12-25 19:57:53 +0300

    methods start corrected

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.time.LocalDate;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static boolean checkingLeapYear(int date) {// check year
+    public static boolean verifyForALeapYear(int date) {
         if (date < 1584) {
             return false;
         } else if (date % 400 == 0 || (date % 4 == 0 && date % 100 != 0)) {
@@ -13,13 +13,12 @@ public class Main {
         }
     }
 
-    public static void CheckingUserPhone(int OS, int year) {
-        int currentYear = LocalDate.now().getYear();
-        if (year >= currentYear && OS == 0) {
+    public static void CheckingTheOSAndYear(int OS, int year) {
+        if (year >= 2015 && OS == 0) {
             System.out.println("Установите версию приложения для iOS по ссылке");
         } else if (OS == 0) {
             System.out.println("Установите облегченную версию приложения для iOS по ссылке");
-        } else if (year >= currentYear && OS == 1) {
+        } else if (year >= 2015 && OS == 1) {
             System.out.println("Установите версию приложения для Android по ссылке");
         } else if (OS == 1) {
             System.out.println("Установите облегченную версию приложения для Android по ссылке");
@@ -41,7 +40,7 @@ public class Main {
         //задача 1
         System.out.println("задача 1");
         int year = 2021;
-        if (checkingLeapYear(year)) {
+        if (verifyForALeapYear(year)) {
             System.out.println(year + " год является високосным");
         } else {
             System.out.println(year + " год не является високосным");
@@ -51,7 +50,7 @@ public class Main {
         System.out.println("задача 2");
         int clientOS = 0;
         int clientDeviceYear = 2016;
-        CheckingUserPhone(clientOS, clientDeviceYear);
+        CheckingTheOSAndYear(clientOS, clientDeviceYear);
 
         //задача 3
         System.out.println("задача 3");
